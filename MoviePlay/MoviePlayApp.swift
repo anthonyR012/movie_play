ba989fdcc0e66da8129d26c379bead0e8685c55c
Anthony RUbio <rubionn27@gmail.com>
2024-07-24 18:36:32 -0500

    Initial Commit

---
+++ MoviePlay/MoviePlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviePlayApp.swift
+//  MoviePlay
+//
+//  Created by Anthony Rubio on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviePlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
